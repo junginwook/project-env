3da98b978c92871a0c1a39b9b85919a280708075
inwookjung <inwook.jung@kr.doubledown.com>
2023-02-07 01:27:38 +0900

    스크립트 수정

--- cicd/serverDeployScript.groovy
+++ cicd/serverDeployScript.groovy
@@ -90,62 +90,63 @@ pipeline {
                 script {
                     try {
 
-                        sh """
+                        sh '''
                         cd deploy
                         cat > deploy.sh <<-EOF
-#!/bin/bash
-BUILD_PATH=\$(ls /home/ec2-user/deploy/*.jar)
-JAR_NAME=\$(basename \$BUILD_PATH)
-echo "> build 파일명: \$JAR_NAME"
+                        #!/bin/bash
+                        BUILD_PATH=$(ls /home/ec2-user/deploy/*.jar)
+                        JAR_NAME=$(basename $BUILD_PATH)
+                        echo "> build 파일명: $JAR_NAME"
+                        
+                        echo "> build 파일 복사"
+                        DEPLOY_PATH=/home/ec2-user/
+                        cp $BUILD_PATH $DEPLOY_PATH
+                        
+                        echo "> springboot-deploy.jar 교체"
+                        CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
+                        APPLICATION_JAR_NAME=springboot-deploy.jar
+                        APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
+                        
+                        ln -Tfs $CP_JAR_PATH $APPLICATION_JAR
+                        
+                        echo "> 현재 실행중인 애플리케이션 pid 확인"
+                        CURRENT_PID=$(pgrep -f $APPLICATION_JAR_NAME)
+                        
+                        if [ -z $CURRENT_PID ]
+                        then
+                          echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+                        else
+                          echo "> kill -15 $CURRENT_PID"
+                          kill -15 $CURRENT_PID
+                          sleep 5
+                        fi
+                        
+                        echo "> $APPLICATION_JAR 배포"
+                        nohup java -jar $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
+                        '''.stripIndent()
 
-echo "> build 파일 복사"
-DEPLOY_PATH=/home/ec2-user/
-cp \$BUILD_PATH \$DEPLOY_PATH
-
-echo "> springboot-deploy.jar 교체"
-CP_JAR_PATH=\$DEPLOY_PATH\$JAR_NAME
-APPLICATION_JAR_NAME=springboot-deploy.jar
-APPLICATION_JAR=\$DEPLOY_PATH\$APPLICATION_JAR_NAME
-
-ln -Tfs \$CP_JAR_PATH \$APPLICATION_JAR
-
-echo "> 현재 실행중인 애플리케이션 pid 확인"
-CURRENT_PID=\$(pgrep -f \$APPLICATION_JAR_NAME)
-
-if [ -z \$CURRENT_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -15 \$CURRENT_PID"
-  kill -15 \$CURRENT_PID
-  sleep 5
-fi
-
-echo "> \$APPLICATION_JAR 배포"
-nohup java -jar \$APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
-"""
 
                         sh """
                         cd deploy
                         cat>appspec.yml<<-EOF
-version: 0.0
-os: linux
-files:
-  - source:  /
-    destination: /home/ec2-user/deploy
-
-permissions:
-  - object: /
-    pattern: "**"
-    owner: ec2-user
-    group: ec2-user
-
-hooks:
-  ApplicationStart:
-    - location: deploy.sh
-      timeout: 60
-      runas: root
-"""
+                        version: 0.0
+                        os: linux
+                        files:
+                          - source:  /
+                            destination: /home/ec2-user/deploy
+                        
+                        permissions:
+                          - object: /
+                            pattern: "**"
+                            owner: ec2-user
+                            group: ec2-user
+                        
+                        hooks:
+                          ApplicationStart:
+                            - location: deploy.sh
+                              timeout: 60
+                              runas: root
+                        """.stripIndent()
 
                         sh """
                         cd deploy
